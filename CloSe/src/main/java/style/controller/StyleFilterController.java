37686637da508a86ad399c96573e6652bc0e7fc8
howl01 <alsgur8463@naver.com>
2023-12-27 12:00:18 +0900

    final

--- CloSe/src/main/java/style/controller/StyleFilterController.java
+++ CloSe/src/main/java/style/controller/StyleFilterController.java
@@ -1,10 +1,12 @@
 package style.controller;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.json.simple.JSONArray;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -21,65 +23,67 @@ import style.model.StyleDao;
 
 @Controller
 public class StyleFilterController {
-	
-	private final String command = "/styleFilter.style";
-	
-	@Autowired
-	private StyleDao styleDao;
-	
-	@RequestMapping(command)
-	@ResponseBody
-	public ResponseEntity<org.json.simple.JSONArray> styleFilter(
-	        @RequestParam(value = "seasonArray", required = false) List<String> seasonLists,
-	        @RequestParam(value = "genderArray", required = false) List<String> genderLists,
-	        @RequestParam(value = "styleArray", required = false) List<String> styleLists,
-	        @RequestParam(value = "temp", required = false) String temp,
-	        Model model) {
-		
-		if(seasonLists != null) {
-			for(String z : seasonLists) {
-				System.out.println("z : " + z);
-			}
-		}
-		if(genderLists != null) {
-			for(String x : genderLists) {
-				System.out.println("x : " + x);
-			}
-		}
-		if(styleLists != null) {
-			for(String y : styleLists) {
-				System.out.println("y : " + y);
-			}
-		}
-		
-	    Map<String, Object> map = new HashMap<String, Object>();
-	    map.put("seasonLists", seasonLists);
-	    map.put("genderLists", genderLists);
-	    map.put("styleLists", styleLists);
-	    map.put("temp", temp);
+   
+   private final String command = "/styleFilter.style";
+   
+   @Autowired
+   private StyleDao styleDao;
+   
+   @RequestMapping(command)
+   @ResponseBody
+   public void styleFilter(
+           @RequestParam(value = "seasonArray", required = false) List<String> seasonLists,
+           @RequestParam(value = "genderArray", required = false) List<String> genderLists,
+           @RequestParam(value = "styleArray", required = false) List<String> styleLists,
+           @RequestParam(value = "temp", required = false) String temp,
+           Model model,
+           HttpServletResponse response) throws IOException {
+      
+      if(seasonLists != null) {
+         for(String z : seasonLists) {
+            System.out.println("z : " + z);
+         }
+      }
+      if(genderLists != null) {
+         for(String x : genderLists) {
+            System.out.println("x : " + x);
+         }
+      }
+      if(styleLists != null) {
+         for(String y : styleLists) {
+            System.out.println("y : " + y);
+         }
+      }
+      
+       Map<String, Object> map = new HashMap<String, Object>();
+       map.put("seasonLists", seasonLists);
+       map.put("genderLists", genderLists);
+       map.put("styleLists", styleLists);
+       map.put("temp", temp);
 
-	    List<StyleBean> lists = styleDao.styleFilter(map);
+       List<StyleBean> lists = styleDao.styleFilter(map);
 
-	    System.out.println("전체 lists 크기: " + lists.size());
+       System.out.println("전체 lists 크기: " + lists.size());
 
-	    org.json.simple.JSONArray jsonArr = new org.json.simple.JSONArray();
-	    for(StyleBean styleBean : lists) {
-	    	System.out.println("title : " + styleBean.getTitle());
-	    	System.out.println("style_number : " + styleBean.getStyle_number());
-	        org.json.simple.JSONObject jsonObj = new org.json.simple.JSONObject();
-	        jsonObj.put("style_number", styleBean.getStyle_number());
-	        jsonObj.put("avg_temperature", styleBean.getAvg_temperature());
-	        jsonObj.put("image1", styleBean.getImage1());
-	        jsonObj.put("title", styleBean.getTitle());
-	        jsonObj.put("style", styleBean.getStyle());
-	        jsonObj.put("member_id", styleBean.getMember_id());
-	        jsonObj.put("nickname", styleBean.getNickname());
-	        jsonObj.put("gender", styleBean.getGender());
-	        jsonArr.add(jsonObj);
-	    }
-	    
-	    HttpHeaders headers = new HttpHeaders();
-	    headers.setContentType(MediaType.APPLICATION_JSON);
-	    return new ResponseEntity<org.json.simple.JSONArray>(jsonArr, headers, HttpStatus.OK);
-	}
+       org.json.simple.JSONArray jsonArr = new org.json.simple.JSONArray();
+       for(StyleBean styleBean : lists) {
+          System.out.println("title : " + styleBean.getTitle());
+          System.out.println("style_number : " + styleBean.getStyle_number());
+           org.json.simple.JSONObject jsonObj = new org.json.simple.JSONObject();
+           jsonObj.put("style_number", styleBean.getStyle_number());
+           jsonObj.put("avg_temperature", styleBean.getAvg_temperature());
+           jsonObj.put("image1", styleBean.getImage1());
+           jsonObj.put("title", styleBean.getTitle());
+           jsonObj.put("style", styleBean.getStyle());
+           jsonObj.put("member_id", styleBean.getMember_id());
+           jsonObj.put("nickname", styleBean.getNickname());
+           jsonObj.put("gender", styleBean.getGender());
+           jsonArr.add(jsonObj);
+       }
+       
+       response.setCharacterEncoding("UTF-8");
+       response.setContentType("text/html");
+       response.getWriter().append(jsonArr.toString());
+       
+   }
 }
